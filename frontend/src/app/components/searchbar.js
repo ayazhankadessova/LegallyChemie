0c798d003795723eaca81d5ea9a51eb43c895ddf
jk021227 <yk2606@nyu.edu>
2024-11-13 18:33:49 -0500

    fixed

--- frontend/src/app/components/searchbar.js
+++ frontend/src/app/components/searchbar.js
@@ -13,6 +13,7 @@ const SearchBar = ({ onProductAdded, isThemeChanged, day }) => {
         
         fetch(`http://localhost:8000/${day}/products/`, {
             method: 'POST',
+            credentials: 'include',
             headers: {
                 'Content-Type': 'application/json',
             },
