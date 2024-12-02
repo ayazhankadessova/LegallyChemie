ace7a42bf6e99a0a5be88632f4db18e7f604e0b9
Sami Hassan <sah8857@nyu.edu>
2024-12-02 17:11:45 -0500

    function to update ratings

--- backend/ratings.py
+++ backend/ratings.py
@@ -11,4 +11,20 @@ from bson import ObjectId
 class RatingsManager:
     def __init__(self, users_collection):
         self.users_collection = users_collection
-        
+
+    #function to update products rating
+    def add_rating(self, product_id: ObjectId, rating: int) -> bool:
+        update_result = self.products_collection.update_one(
+            {"_id": product_id},
+            {
+                "$inc": {"ratings.totalRating": rating, "ratings.ratingCount": 1},
+                "$set": {"ratings.averageRating": {
+                    "$round": [
+                        {"$divide": ["$ratings.totalRating", "$ratings.ratingCount"]},
+                        2
+                    ]
+                }}
+            }
+        )
+        return update_result.modified_count > 0
+    
