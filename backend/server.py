6674c5e2cb3d07e298fb3473a69b426ed97e8e91
John Yun Moe <jy4017@nyu.edu>
2024-12-03 18:29:26 -0500

    changes for deployment

--- backend/server.py
+++ backend/server.py
@@ -65,7 +65,7 @@ ingredients_collection = db.get_collection("ingredients")
 """
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://your-frontend.vercel.app"],
+    allow_origins=["http://localhost:3000", "https://legally-chemie.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
